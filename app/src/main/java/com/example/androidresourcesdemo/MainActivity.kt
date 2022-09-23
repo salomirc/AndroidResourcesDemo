a4b00a6ab12aa1376cf39818dc4b651ade30b6c0
Ciprian Salomir <ciprian.salomir@gmail.com>
2022-09-23 20:25:21 +0300

    DataBinding setup updated

--- app/src/main/java/com/example/androidresourcesdemo/MainActivity.kt
+++ app/src/main/java/com/example/androidresourcesdemo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.androidresourcesdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.androidresourcesdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Inflate view and obtain an instance of the binding class.
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
     }
 }
